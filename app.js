78e5ba1c2f1813ed6ec4d90b5c336c86cd913158
Eric Ankarstran <eankarst@uncc.edu>
2020-12-18 22:48:44 -0500

    Changed header to reflect server

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const jwtMW = exjwt({
 var currentUsername = '';
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://157.245.210.68:3000');
     res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization');
     next();
 });
